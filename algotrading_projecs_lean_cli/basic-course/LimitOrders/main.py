90ab0e11072924b52d7ea43e8d45f7d40b4111f6
hsm207 <hsm207@users.noreply.github.com>
2023-03-18 23:34:11 +0000

    Format code
    Signed-off-by: hsm207 <hsm207@users.noreply.github.com>

--- algotrading_projecs_lean_cli/basic-course/LimitOrders/main.py
+++ algotrading_projecs_lean_cli/basic-course/LimitOrders/main.py
@@ -6,18 +6,19 @@ class LimitOrders(QCAlgorithm):
     This algo buys one stock using a limit order and then immediately sets another
     limit order to sell the stock at a higher price
     """
+
     def Initialize(self):
         self.SetStartDate(2011, 1, 1)  # Set Start Date
         # no end date means the algo will run forever
         # self.SetEndDate(2013, 10, 11)  # Set End Date
         self.SetCash(100000)  # Set Strategy Cash
         self.stock = self.AddEquity("TSLA", Resolution.Daily)
-        self.invest= True
+        self.invest = True
 
     def OnData(self, data):
         """OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
-            Arguments:
-                data: Slice object keyed by symbol containing the stock data
+        Arguments:
+            data: Slice object keyed by symbol containing the stock data
         """
         ticker = self.stock.Symbol.Value
 
